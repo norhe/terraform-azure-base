6a96acb17bd89a10f782e6464e2a147d08511c03
Ehron <ehron@ehron.org>
2020-03-17 17:39:40 -0400

    add outputs

--- variables.tf
+++ variables.tf
@@ -17,3 +17,11 @@ variable "dest-range" {
 output "rg-id" {
     value = azurerm_resource_group.rg.id
 }
+
+output "rg-name" {
+    value = azurerm_resource_group.rg.name
+}
+
+output "rg-name" {
+    value = azurerm_resource_group.rg.location
+}
